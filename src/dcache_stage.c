c56355c402490801570b6b18d85850fb579fa3b8
Jonathan Michel <jomichel@ucsc.edu>
2024-12-01 18:39:25 -0800

    Wrapping up

--- src/dcache_stage.c
+++ src/dcache_stage.c
@@ -26,7 +26,6 @@
  * Description  :
  ***************************************************************************************/
 
-#include "dcache_stage_log.h"
 #include "debug/debug_macros.h"
 #include "debug/debug_print.h"
 #include "globals/assert.h"
@@ -62,8 +61,8 @@
 /**************************************************************************************/
 /* Global Variables */
 
-Dcache_Stage* dc       = NULL;
-CCacheState   logState = NULL;
+Dcache_Stage* dc = NULL;
+
 /**************************************************************************************/
 /* set_dcache_stage: */
 
@@ -368,8 +367,6 @@ void update_dcache_stage(Stage_Data* src_sd) {
         wake_up_ops(op, REG_DATA_DEP, model->wake_hook);
       }
     } else {  // data cache miss
-
-
       if(op->table_info->mem_type == MEM_ST)
         STAT_EVENT(op->proc_id, POWER_DCACHE_WRITE_MISS);
       else
@@ -380,9 +377,9 @@ void update_dcache_stage(Stage_Data* src_sd) {
 
       if(op->table_info->mem_type == MEM_LD) {  // load request
         if(((model->mem == MODEL_MEM) &&
-            scan_stores(op->oracle_info.va,
-                        op->oracle_info.mem_size))) {  // scan the store
-                                                       // forwarding buffer
+            scan_stores(
+              op->oracle_info.va,
+              op->oracle_info.mem_size))) {  // scan the store forwarding buffer
           if(!op->off_path) {
             STAT_EVENT(op->proc_id, DCACHE_ST_BUFFER_HIT);
             STAT_EVENT(op->proc_id, DCACHE_ST_BUFFER_HIT_ONPATH);
@@ -429,39 +426,6 @@ void update_dcache_stage(Stage_Data* src_sd) {
           }
 
           if(!op->off_path) {
-            printf("1\n");
-            if(logState == NULL) {
-              logState = (CCacheState)malloc(sizeof(CCacheState));
-              dcacheLogInit(logState);
-            }
-            if(!line) {  // line is NULL on cache miss
-              uns64 repl_line_addr;
-              // Get replacement line address using get_next_repl_line
-              Flag repl_line_valid;
-              printf("DCACHE MISS\n");
-              void* data = get_next_repl_line(&dc->dcache, dc->proc_id,
-                                              line_addr, &repl_line_addr,
-                                              &repl_line_valid);
-              (void)data;
-              EvictType result;
-              printf("DCACHE MI1SS\n");
-
-              result = dcacheLogInsert(logState, 0, line_addr, repl_line_addr,
-                                       dc->dcache.num_lines);
-              switch(result) {
-                case CAPACITY:
-                  STAT_EVENT(dc->proc_id, DCACHE_CAPACITY_MISS);
-                  break;
-                case CONFLICT:
-                  STAT_EVENT(dc->proc_id, DCACHE_CONFLICT_MISS);
-                  break;
-                case COMPULSORY:
-                  STAT_EVENT(dc->proc_id, DCACHE_COMPULSORY_MISS);
-                  break;
-                default:
-                  break;
-              }
-            }
             STAT_EVENT(op->proc_id, DCACHE_MISS);
             STAT_EVENT(op->proc_id, DCACHE_MISS_ONPATH);
             STAT_EVENT(op->proc_id, DCACHE_MISS_LD_ONPATH);
@@ -517,41 +481,6 @@ void update_dcache_stage(Stage_Data* src_sd) {
           }
 
           if(!op->off_path) {
-                        printf("2\n");
-
-            if(logState == NULL) {
-              logState = (CCacheState)malloc(sizeof(CCacheState));
-              dcacheLogInit(logState);
-            }
-            if(!line) {  // line is NULL on cache miss
-              uns64 repl_line_addr;
-              // Get replacement line address using get_next_repl_line
-              Flag repl_line_valid;
-              printf("DCACHE MISS\n");
-              void* data = get_next_repl_line(&dc->dcache, dc->proc_id,
-                                              line_addr, &repl_line_addr,
-                                              &repl_line_valid);
-              (void)data;
-              EvictType result;
-              printf("DCACHE MI1SS\n");
-
-              result = dcacheLogInsert(logState, 0, line_addr, repl_line_addr,
-                                       dc->dcache.num_lines);
-              switch(result) {
-                case CAPACITY:
-                  STAT_EVENT(dc->proc_id, DCACHE_CAPACITY_MISS);
-                  break;
-                case CONFLICT:
-                  STAT_EVENT(dc->proc_id, DCACHE_CONFLICT_MISS);
-                  break;
-                case COMPULSORY:
-                  STAT_EVENT(dc->proc_id, DCACHE_COMPULSORY_MISS);
-                  break;
-                default:
-                  break;
-              }
-            }
-            
             STAT_EVENT(op->proc_id, DCACHE_MISS);
             STAT_EVENT(op->proc_id, DCACHE_MISS_ONPATH);
             STAT_EVENT(op->proc_id, DCACHE_MISS_LD_ONPATH);
@@ -609,40 +538,7 @@ void update_dcache_stage(Stage_Data* src_sd) {
               STAT_EVENT_ALL(ONE_MORE_DISCARDED_L0CACHE);
           }
 
-          if(!op->off_path) {            printf("3\n");
-
-            if(logState == NULL) {
-              logState = (CCacheState)malloc(sizeof(CCacheState));
-              dcacheLogInit(logState);
-            }
-            if(!line) {  // line is NULL on cache miss
-              uns64 repl_line_addr;
-              // Get replacement line address using get_next_repl_line
-              Flag repl_line_valid;
-              printf("DCACHE MISS\n");
-              void* data = get_next_repl_line(&dc->dcache, dc->proc_id,
-                                              line_addr, &repl_line_addr,
-                                              &repl_line_valid);
-              (void)data;
-              EvictType result;
-              printf("DCACHE MI1SS\n");
-
-              result = dcacheLogInsert(logState, 0, line_addr, repl_line_addr,
-                                       dc->dcache.num_lines);
-              switch(result) {
-                case CAPACITY:
-                  STAT_EVENT(dc->proc_id, DCACHE_CAPACITY_MISS);
-                  break;
-                case CONFLICT:
-                  STAT_EVENT(dc->proc_id, DCACHE_CONFLICT_MISS);
-                  break;
-                case COMPULSORY:
-                  STAT_EVENT(dc->proc_id, DCACHE_COMPULSORY_MISS);
-                  break;
-                default:
-                  break;
-              }
-            }
+          if(!op->off_path) {
             STAT_EVENT(op->proc_id, DCACHE_MISS);
             STAT_EVENT(op->proc_id, DCACHE_MISS_ONPATH);
             STAT_EVENT(op->proc_id, DCACHE_MISS_ST_ONPATH);
@@ -712,8 +608,6 @@ Flag dcache_fill_line(Mem_Req* req) {
   /* if it can't get a write port, fail */
   if(!get_write_port(&dc->ports[bank])) {
     cycle_count = old_cycle_count;
-    STAT_EVENT(dc->proc_id,
-               DCACHE_FILL_PORT_UNAVAILABLE_ONPATH + req->off_path);
     return FAILURE;
   }
 
@@ -728,7 +622,8 @@ Flag dcache_fill_line(Mem_Req* req) {
           (int)(req->addr >> LOG2(DCACHE_LINE_SIZE)), req->op_count,
           (req->op_count ? req->oldest_op_unique_num : -1));
 
-    ASSERT(dc->proc_id, cycle_count >= req->emitted_cycle);
+    data = (Dcache_Data*)cache_insert(&dc->pref_dcache, dc->proc_id, req->addr,
+                                      &line_addr, &repl_line_addr);
     // mark the data as HW_prefetch if prefetch mark it as
     // fetched_by_offpath if off_path this is done downstairs
   } else {
@@ -776,17 +671,9 @@ Flag dcache_fill_line(Mem_Req* req) {
       STAT_EVENT(dc->proc_id, DCACHE_WB_REQ_DIRTY);
       STAT_EVENT(dc->proc_id, DCACHE_WB_REQ);
     }
-    if(logState == NULL) {
-      logState = (CCacheState)malloc(sizeof(CCacheState));
-      dcacheLogInit(logState);
-    }
-    dcacheLogEvictCacheLine(logState, repl_line_addr, dc->dcache.num_lines);
- 
-    STAT_EVENT(dc->proc_id, DCACHE_PROBE);
 
     data = (Dcache_Data*)cache_insert(&dc->dcache, dc->proc_id, req->addr,
                                       &line_addr, &repl_line_addr);
-
     DEBUG(dc->proc_id,
           "Filling dcache  off_path:%d addr:0x%s  :%7d index:%7d op_count:%d "
           "oldest:%lld\n",
@@ -794,24 +681,6 @@ Flag dcache_fill_line(Mem_Req* req) {
           (int)(req->addr >> LOG2(DCACHE_LINE_SIZE)), req->op_count,
           (req->op_count ? req->oldest_op_unique_num : -1));
     STAT_EVENT(dc->proc_id, DCACHE_FILL);
-    ASSERT(dc->proc_id, req->emitted_cycle);
-    ASSERT(dc->proc_id, cycle_count >= req->emitted_cycle);
-    ASSERT(dc->proc_id, ((int)req->mlc_hit + (int)req->l1_hit) < 2);
-    INC_STAT_EVENT(dc->proc_id, DATA_LD_CYCLES_ONPATH + req->off_path,
-                   cycle_count - req->emitted_cycle);
-    if(req->mlc_hit) {
-      STAT_EVENT(dc->proc_id, DATA_LD_MLC_ACCESSES_ONPATH + req->off_path);
-      INC_STAT_EVENT(dc->proc_id, DATA_LD_MLC_CYCLES_ONPATH + req->off_path,
-                     cycle_count - req->emitted_cycle);
-    } else if(req->l1_hit) {
-      STAT_EVENT(dc->proc_id, DATA_LD_L1_ACCESSES_ONPATH + req->off_path);
-      INC_STAT_EVENT(dc->proc_id, DATA_LD_L1_CYCLES_ONPATH + req->off_path,
-                     cycle_count - req->emitted_cycle);
-    } else {
-      STAT_EVENT(dc->proc_id, DATA_LD_MEM_ACCESSES_ONPATH + req->off_path);
-      INC_STAT_EVENT(dc->proc_id, DATA_LD_MEM_CYCLES_ONPATH + req->off_path,
-                     cycle_count - req->emitted_cycle);
-    }
   }
 
   /* set up dcache line fields */
@@ -824,12 +693,12 @@ Flag dcache_fill_line(Mem_Req* req) {
   data->misc_state         = req->off_path | req->off_path << 1;
   data->fetched_by_offpath = USE_CONFIRMED_OFF ? req->off_path_confirmed :
                                                  req->off_path;
-  data->offpath_op_addr    = req->oldest_op_addr;
-  data->offpath_op_unique  = req->oldest_op_unique_num;
-  data->fetch_cycle        = cycle_count;
-  data->onpath_use_cycle   = (req->type == MRT_DPRF || req->off_path) ?
-                               0 :
-                               cycle_count;
+  data->offpath_op_addr   = req->oldest_op_addr;
+  data->offpath_op_unique = req->oldest_op_unique_num;
+  data->fetch_cycle       = cycle_count;
+  data->onpath_use_cycle  = (req->type == MRT_DPRF || req->off_path) ?
+                             0 :
+                             cycle_count;
 
   wp_process_dcache_fill(data, req);
 
--- src/prefetcher/pref_bof.cc
+++ src/prefetcher/pref_bof.cc
@@ -1,12 +1,14 @@
-
+#include <algorithm>
+#include <iostream>
 #include <unordered_map>
-
+#include <vector>
 
 #include "debug/debug_macros.h"
 #include "debug/debug_print.h"
 #include "globals/global_defs.h"
 #include "globals/global_types.h"
 #include "globals/global_vars.h"
+#include "prefetcher/pref_bof.param.h"
 
 #include "globals/assert.h"
 #include "globals/utils.h"
@@ -22,155 +24,265 @@
 #include "memory/memory.h"
 #include "memory/memory.param.h"
 #include "prefetcher/l2l1pref.h"
-#include "statistics.h"
-#include <iostream>
+#include "prefetcher/pref.param.h"
 #include "prefetcher/pref_bof.h"
+#include "prefetcher/pref_bof.param.h"
+#include "statistics.h"
+#include "globals/global_defs.h"
+#include "globals/global_types.h"
+#include "globals/global_vars.h"
+
 struct RRentry {
   uint64_t base;
   uint64_t offset;
   uint64_t score;
 };
 
+#include <cmath>
+#include <cstdint>
+#include <unordered_map>
+#include <vector>
 
 class BOFprefetcher {
  private:
+  struct RRentry {
+    uint64_t base;
+    uint64_t offset;
+    uint64_t score;
+    uint64_t last_access_time;
+    uint64_t tag;
+  };
+  uint64_t                               active_offset = 0;
+  uint64_t                               MXOFFSET = VA_PAGE_SIZE_BYTES / DCACHE_LINE_SIZE ;
   std::unordered_map<uint64_t, RRentry> RRtable;
-  uint64_t best_offset;
-  uint64_t best_score;
-  bool learning;
-  uint64_t last_miss_addr;
-  uint64_t new_entry_base;
-  HWP_Info* hwp_info;  // This needs to be public or accessed via a getter
 
-  const uint64_t BAD_SCORE = 8;
+  std::vector<uint64_t>                  offset_list;
+  std::unordered_map<uint64_t, uint64_t> score_table;
+  uint64_t                               best_offset;
+  bool                                   have_learned = false;
+  uint64_t                               last_miss_addr;
+  uint64_t                               new_entry_base;
+  uint64_t                               current_offset_index;
+  uint64_t                               round_count;
+  uint64_t       cycle_count;  
+  const uint64_t BAD_SCORE = PREF_BOF_BADSCORE;
+  const uint64_t SCOREMAX  = 32;
+  const uint64_t ROUNDMAX  = 128;
+  const uint64_t RRMAX    = 256;
 
  public:
+  uint64_t  best_score;
+  HWP_Info* hwp_info;
+  uint64_t  type;
+
+  BOFprefetcher() :
+      best_offset(0) , last_miss_addr(0), new_entry_base(0),
+      current_offset_index(0), round_count(0), cycle_count(0), best_score(0),
+      hwp_info(nullptr) {
+    initialize_offset_list();
+  }
+
   BOFprefetcher(HWP* hwp) :
-    best_offset(0),
-    best_score(0), 
-    learning(true),
-    last_miss_addr(0),
-    new_entry_base(0),
-    hwp_info(hwp->hwp_info) {}
+      best_offset(0)   , last_miss_addr(0), new_entry_base(0),
+      current_offset_index(0), round_count(0), cycle_count(0), best_score(0),
+      hwp_info(hwp->hwp_info) {
+    initialize_offset_list();
+  }
 
-  // Add getter for hwp_info
-  HWP_Info* get_hwp_info() const { return hwp_info; }
-
-  void train_miss(uint64_t miss_addr) {
-    if (learning) {
-      if (new_entry_base == 0) {
-        new_entry_base = miss_addr;
-      } else {
-        uint64_t offset = miss_addr - new_entry_base;
-        if (RRtable.find(offset) != RRtable.end()) {
-          RRtable[offset].score++;
-          if (RRtable[offset].score > best_score) {
-            best_score = RRtable[offset].score;
-            best_offset = offset;
-          }
-        } else {
-          RRtable[offset] = {new_entry_base, offset, 1};
-        }
-        new_entry_base = miss_addr;
-      }
-    } else {
-      if (last_miss_addr != 0) {
-        uint64_t offset = miss_addr - last_miss_addr;
-        if (RRtable.find(offset) != RRtable.end()) {
-          RRtable[offset].score++;
-          if (RRtable[offset].score > best_score) {
-            best_score = RRtable[offset].score;
-            best_offset = offset;
-          }
-        }
-      }
-      last_miss_addr = miss_addr;
+  BOFprefetcher& operator=(const BOFprefetcher& other) {
+    if(this != &other) {
+      this->RRtable              = other.RRtable;
+      this->best_offset          = other.best_offset;
+      this->best_score           = other.best_score;
+       this->last_miss_addr       = other.last_miss_addr;
+      this->new_entry_base       = other.new_entry_base;
+      this->hwp_info             = other.hwp_info;
+      this->current_offset_index = other.current_offset_index;
+      this->round_count          = other.round_count;
+      this->offset_list          = other.offset_list;
+      this->score_table          = other.score_table;
+      this->cycle_count          = other.cycle_count;
     }
+    return *this;
+  }
 
-    if (best_score > BAD_SCORE) {
-      learning = false;
+  void initialize_offset_list() {
+    offset_list = {1,   2,   3,   4,   5,   6,   8,   9,   10,  12,  15,
+                   16,  18,  20,  24,  25,  27,  30,  32,  36,  40,  45,
+                   48,  50,  54,  60,  65,  72,  75,  80,  81,  90,  96,
+                   100, 108, 120, 125, 128, 135, 144, 150, 160, 162, 180,
+                   192, 200, 216, 225, 240, 243, 250, 256};
+    // // remove offsets greater than MXOFFSET
+    // offset_list.erase(std::remove_if(offset_list.begin(), offset_list.end(),
+    //                                  [this](uint64_t offset) {
+    //                                    return offset > MXOFFSET;
+    //                                  }),
+    //                   offset_list.end());
+    // Initialize the score table
+    for(auto offset : offset_list) {
+      score_table[offset] = 0;
     }
   }
 
-  void train_hit(uint64_t hit_addr) {
-    if (last_miss_addr != 0) {
-      uint64_t offset = hit_addr - last_miss_addr;
-      if (RRtable.find(offset) != RRtable.end()) {
-        RRtable[offset].score++;
-      }
+void train_miss(uint64_t miss_addr, uint64_t cycle_count) {
+
+     
+    
+    if (current_offset_index >= offset_list.size()) {
+      //  std::cerr << "Invalid offset index: " << current_offset_index << std::endl;
+        return;
     }
+    uint64_t current_offset = offset_list[current_offset_index];
+    uint64_t test_addr = miss_addr - current_offset;
+ 
+     if (RRtable.find(test_addr) != RRtable.end()    ) {
+        uint64_t access_time = cycle_count - RRtable[test_addr].last_access_time;
+       std::cout << "Hit at offset " << current_offset << " with access time " << access_time << std::endl;
+
+        if (access_time >= 100) {
+            score_table[current_offset]++;
+            if (score_table[current_offset] > best_score) {
+                best_score = score_table[current_offset];
+                best_offset = current_offset;
+            }
+        }
+    }
+
+    current_offset_index = (current_offset_index + 1) % offset_list.size();
+    if (current_offset_index == 0) {
+        round_count++;
+    }
+
+    if (best_score >= SCOREMAX || round_count >= ROUNDMAX) {
+        have_learned = true;
+        if (best_score < BAD_SCORE) {
+            best_offset = 0;
+        }
+        // Debugging outputs
+        for (auto& entry : score_table) {
+            std::cout << "Offset: " << entry.first << " Score: " << entry.second << std::endl;
+        }
+      
+        active_offset = best_offset;
+        best_offset = 0;
+       std::cout << "Learning phase completed with best offset: " << active_offset << std::endl;
+        reset();
+    }
+
+    RRentry new_entry;
+    new_entry.base = miss_addr - current_offset;
+    new_entry.offset = current_offset;
+    new_entry.score = score_table[current_offset];
+    new_entry.last_access_time = cycle_count;
+ 
+    RRtable[miss_addr] = new_entry;
+
+    last_miss_addr = miss_addr;
+}
+ bool is_same_page(uint64_t addr1, uint64_t addr2) {
+    return (addr1 / VA_PAGE_SIZE_BYTES) == (addr2 / VA_PAGE_SIZE_BYTES);
   }
+  void train_hit(uint64_t hit_addr) {}
 
   uint64_t getPrefAddr(uint64_t miss_addr) {
-    if (learning) {
+    if(!have_learned)
       return 0;
-    } else {
-      return miss_addr + best_offset;
-    }
+    if (active_offset == 0)
+      return 0;
+    // if (!is_same_page(miss_addr, last_miss_addr))
+    //   return 0;
+     return miss_addr + (active_offset);
   }
 
-  void printRRtable() {
-    std::cout << "RR Table:\n";
-    for (const auto& entry : RRtable) {
-      std::cout << "Offset: " << entry.second.offset
-                << ", Base: " << entry.second.base
-                << ", Score: " << entry.second.score << "\n";
+  void reset() {
+    RRtable.clear();
+    best_score = 0;
+    round_count = 0;
+    current_offset_index = 0;
+ 
+    for (auto& entry : score_table) {
+        entry.second = 0;
     }
-  }
+}
+  
 };
-static BOFprefetcher* g_bof = nullptr;
+
+typedef struct {
+  BOFprefetcher* bof_hwp_core_ul1;
+  BOFprefetcher* bof_hwp_core_umlc;
+} bof_prefetchers;
+
+bof_prefetchers bof_prefetchers_array;
+
+void init_bof(HWP* hwp, BOFprefetcher* bof_hwp_core) {
+  for(uns i = 0; i < NUM_CORES; i++) {
+    bof_hwp_core[i]                   = BOFprefetcher(hwp);
+    bof_hwp_core[i].hwp_info          = hwp->hwp_info;
+    bof_hwp_core[i].hwp_info->enabled = TRUE;
+  }
+}
 
 void pref_bof_init(HWP* hwp) {
-  // Initialize global instance
- 
-  if(g_bof) delete g_bof;
-  g_bof = new BOFprefetcher(hwp);
-  hwp->hwp_info->enabled = true;
- 
-}
+  printf("BAD_SCORE: %d\n", PREF_BOF_BADSCORE);
+  printf("PREF_BOF_ON: %d\n", PREF_BOF_ON);
+  if(!PREF_BOF_ON)
+    return;
+  if(PREF_UMLC_ON) {
+    bof_prefetchers_array.bof_hwp_core_umlc = new BOFprefetcher[NUM_CORES];
+    init_bof(hwp, bof_prefetchers_array.bof_hwp_core_umlc);
+  }
 
-void pref_bof_ul1_miss(uns8 proc_id, Addr lineAddr, Addr loadPC, uns32 global_hist) {
-  if(!g_bof) return;
-  printf("BOF: UL1 miss\n");
-  ASSERT(1, g_bof);
-
-  g_bof->train_miss(lineAddr);
-  auto pref_addr = g_bof->getPrefAddr(lineAddr);
-  if (pref_addr != 0) {
-    if (!pref_dl0req_queue_filter(pref_addr)) {
-      pref_addto_dl0req_queue(proc_id, pref_addr, g_bof->get_hwp_info()->id);
-    }
+  if(PREF_UL1_ON) {
+    bof_prefetchers_array.bof_hwp_core_ul1 = new BOFprefetcher[NUM_CORES];
+    init_bof(hwp, bof_prefetchers_array.bof_hwp_core_ul1);
   }
 }
 
-void pref_bof_ul1_prefhit(uns8 proc_id, Addr lineAddr, Addr loadPC, uns32 global_hist) {
-  if(!g_bof) return;
-  printf("BOF: UL1 prefhit\n");
-  ASSERT(1, g_bof);
-  
-  g_bof->train_hit(lineAddr);
-}
-
-void pref_bof_umlc_miss(uns8 proc_id, Addr lineAddr, Addr loadPC, uns32 global_hist) {
-  printf("BOF: UMLC miss\n");
-  if(!g_bof) return;
-  g_bof->train_miss(lineAddr);
-  auto pref_addr = g_bof->getPrefAddr(lineAddr);
-  if (pref_addr != 0) {
-    if (!pref_dl0req_queue_filter(pref_addr)) {
-      pref_addto_dl0req_queue(proc_id, pref_addr, g_bof->get_hwp_info()->id);
-    }
+void pref_bof_ul1_miss(uns8 proc_id, Addr lineAddr, Addr loadPC,
+                       uns32 global_hist) {
+ 
+  if(!bof_prefetchers_array.bof_hwp_core_ul1)
+    return;
+  bof_prefetchers_array.bof_hwp_core_ul1[proc_id].train_miss(lineAddr, cycle_count);
+  Addr pref_addr = bof_prefetchers_array.bof_hwp_core_ul1[proc_id].getPrefAddr(
+    lineAddr);
+  if(pref_addr != 0) {
+    // Issue prefetch
+    // if(!pref_addto_ul1req_queue(
+    //      proc_id, pref_addr,
+    //      bof_prefetchers_array.bof_hwp_core_ul1[proc_id].hwp_info->id)) {
+    //   std::cout << "Prefetch queue is full\n";
+    // }
   }
 }
 
-void pref_bof_umlc_prefhit(uns8 proc_id, Addr lineAddr, Addr loadPC, uns32 global_hist) {
-  printf("BOF: UMLC prefhit\n");
-  if(!g_bof) return;
-  g_bof->train_hit(lineAddr);
+void pref_bof_ul1_prefhit(uns8 proc_id, Addr lineAddr, Addr loadPC,
+                          uns32 global_hist) {
+  if(!bof_prefetchers_array.bof_hwp_core_ul1)
+    return;
+  bof_prefetchers_array.bof_hwp_core_ul1[proc_id].train_miss(lineAddr, cycle_count);
 }
 
-// Add cleanup function
-void pref_bof_cleanup() {
-  delete g_bof;
-  g_bof = nullptr;
+void pref_bof_umlc_miss(uns8 proc_id, Addr lineAddr, Addr loadPC,
+                        uns32 global_hist) {
+  if(!bof_prefetchers_array.bof_hwp_core_umlc)
+    return;
+  bof_prefetchers_array.bof_hwp_core_umlc[proc_id].train_miss(lineAddr, cycle_count);
+  Addr pref_addr = bof_prefetchers_array.bof_hwp_core_umlc[proc_id].getPrefAddr(
+    lineAddr);
+  if(pref_addr != 0) {
+    // // Issue prefetch
+    // if(!pref_addto_umlc_req_queue(
+    //      proc_id, pref_addr,
+    //      bof_prefetchers_array.bof_hwp_core_umlc[proc_id].hwp_info->id)) {
+    //   std::cout << "Prefetch queue is full\n";
+    // }
+  }
+}
+
+void pref_bof_umlc_prefhit(uns8 proc_id, Addr lineAddr, Addr loadPC,
+                           uns32 global_hist) {
+  if(!bof_prefetchers_array.bof_hwp_core_umlc)
+    return;
+  bof_prefetchers_array.bof_hwp_core_umlc[proc_id].train_miss(lineAddr, cycle_count);
 }
